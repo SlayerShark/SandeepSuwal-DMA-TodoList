b027d2483739bff0d9e9c181fc9d7fbebf6a22d6
SlayerShark <ssadeep21@tbc.edu.np>
2021-12-18 23:56:36 +0545

    todo model and repository implemented

--- app/src/main/java/com/example/sandeepsuwal_todolist/MainActivity.java
+++ app/src/main/java/com/example/sandeepsuwal_todolist/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.sandeepsuwal_todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    Repository repository;
+    List<Todo> todos;
+    public static final String Tag = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        repository = Repository.getInstance(this);
+        todos = repository.getAllTodos();
+        Log.d(Tag, "todos size: "+ todos.size());
+
+
     }
 }
---
+++ app/src/main/java/com/example/sandeepsuwal_todolist/Repository.java
@@ -0,0 +1,73 @@
+package com.example.sandeepsuwal_todolist;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+public class Repository {
+
+    private static Repository sInstance;    //single instance
+
+    private ArrayList<Todo> todos;
+
+    private Repository(Context context) {
+        todos = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Todo todo = new Todo();
+            todo.setTitle("title" + i);
+            todo.setDescription("description" + i);
+            todo.setPriority(i);
+            todos.add(todo);
+        }
+    }
+
+
+    /*-------------------singleton class---------------------------------*/
+    public static Repository getInstance(Context context) {
+        if (sInstance == null) {
+            sInstance = new Repository(context);
+        }
+        return sInstance;
+    }
+
+    public List<Todo> getAllTodos() {
+        return todos;
+    }
+
+    /*---------------------------------CRUD read data----------------------------------*/
+    public Todo getTodoById(UUID id) {
+        for (int i = 0; i < todos.size(); i++) {
+            Todo todo = todos.get(i);
+            if (todo.getId().equals(id)) {
+                return todo;
+            }
+        }
+        return null;
+    }
+
+    /*---------------------------------CRUD delete data------------------------------*/
+    public void delete(UUID id) {
+/*        for (Todo todo : todos) {
+            if (todo.getId().equals(id)) {
+                todos.remove(todo);
+            }
+        }
+*/
+        Todo todo = getTodoById(id);
+        if (todo != null)
+        todos.remove(todo);
+    }
+
+    /*-------------------------------CRUD update data-------------------------------------*/
+    public Todo update(Todo todo) {
+        Todo newTodo = getTodoById(todo.getId());
+        newTodo.setPriority(todo.getPriority());
+        newTodo.setDescription(todo.getDescription());
+        newTodo.setTitle(todo.getTitle());
+        newTodo.setUpdatedAt(new Date());
+        return newTodo;
+    }
+}
